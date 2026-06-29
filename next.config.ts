18f51c50abc89375eff766d4d3f214f126eebf9e
unrastand <kolawoleisrael2500@gmail.com>
2026-06-29 23:48:02 +0100

    b

--- next.config.ts
+++ next.config.ts
@@ -45,5 +45,8 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+if (process.env.NODE_ENV === "development") {
+  void import("@opennextjs/cloudflare").then(({ initOpenNextCloudflareForDev }) => {
+    initOpenNextCloudflareForDev();
+  });
+}
