704636e71a8cb0c04058b72f331c1bfb5af734d5
unrastand <kolawoleisrael2500@gmail.com>
2026-06-30 18:38:20 +0100

    ,,

--- scripts/_tmp-check-ramp-css.mjs
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "fs";
-
-const c = readFileSync("public/ramp-files/0g8gib8b5vkql.css", "utf8");
-console.log("headline-xs", c.includes("headline-xs"));
-const m = c.match(/\.headline-xs\{[^}]{0,200}/);
-console.log(m ? m[0] : "no headline-xs rule");
-const lausanne = [...c.matchAll(/Lausanne[^"']{0,60}/g)].slice(0, 3);
-console.log("Lausanne refs", lausanne.map((x) => x[0]));
